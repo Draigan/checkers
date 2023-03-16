5fe230f4a3581e0c7b2607d8fa8164b2123bca47
Draigan <draigan.lefebvre@gmail.com>
2023-03-15 20:03:57 -0400

    Somewhere

--- server/index.js
+++ server/index.js
@@ -1,43 +1,84 @@
-const express = require('express')
-const http = require('http')
-const Server = require("socket.io").Server
-const app = express()
-const path = require('path')
-const port = 8080;
+const express = require('express');
+const app = express();
+const PORT = 4001;
 
-const server = http.createServer(app)
-const io = new Server(server, {
+const http = require('http').Server(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
   cors: {
     origin: "*"
   }
-})
-
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname, "../client/build");
-
-app.use(express.static(buildPath))
-
-app.get("/*", function(req, res) {
-
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function(err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-
-})
-
-io.on("connection", (socket) => {
-  console.log("user connected", socket.id);
-  socket.on("send_message", (data) => {
-    console.log(data);
-    io.emit("recieve_message", data)
-  })
-})
-
-server.listen(port, () => {
-  console.log("SERVER IS LISTENING ON PORT", port)
 });
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+  socket.on('test', () => {
+    console.log("EMIT TEST FROM SERVER");
+    io.to("room_one").emit("recieve_message");
+  })
+  socket.on('table_one', () => {
+    console.log(socket.id, "Joined Table One");
+    socket.join("room_one");
+  })
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
+// const express = require('express')
+// const http = require('http')
+// const Server = require("socket.io").Server
+// const app = express()
+// const path = require('path')
+// const port = 8080;
+
+// const server = http.createServer(app)
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*"
+//   }
+// })
+
+// const _dirname = path.dirname("")
+// const buildPath = path.join(_dirname, "../client/build");
+
+// app.use(express.static(buildPath))
+
+// app.get("/*", function(req, res) {
+
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function(err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+
+// })
+
+// io.on("connection", (socket) => {
+//   console.log("user connected", socket.id);
+//   socket.on("test", () => {
+//     console.log("Joined Table One");
+//     // io.emit("recieve_message", data)
+//   })
+// })
+
+// server.listen(port, () => {
+//   console.log("SERVER IS LISTENING ON PORT", port)
+// });
